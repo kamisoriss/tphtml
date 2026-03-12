feffef89b663927d308f9a77b9d23347d7f4d0bd
kamisoriss <kamisoriss@users.noreply.github.com>
2026-03-12 12:39:37 +0100

    fin tp7et 6

---
+++ tp8/header.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="style.css">
+<script src="../script.js" defer></script>
+<button type="button" onclick="darkFunction()" class="darkmode_button"></button>
+<img src="img/logo.png" alt="php">
+ <div class="tp">
+            <nav>
+                <h3>lien vers tp</h3>
+                <ul>
+                    <li><a href="../index.html">*acceuil</a></li>
+                    <li><a href="../tp3/tp3.html">tp3</a></li>
+                    <li><a href="../tp4/acceuiltp4.html">tp4</a></li>
+                    <li><a href="../tp5.html">tp5</a></li>
+                    <li><a href="../tp6/index.html">tp6</a></li>
+                    <li><a href="../tp7/tp7.html"></a></li>
+                    <li><a href="index.php">tp8</a></li>
+                </ul>
+            </nav>
+        </div>
---
+++ tp8/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php include 'header.php'; ?>
+    </header>
+    <p>body</p>
+    <div class="sous-menu">
+        <p>sous menu</p>
+    </div>
+    <footer>
+        <p>footer</p>
+    </footer>
+</body>
+</html>
